a8233d0288c60e3baadd6f3a0e722d2cc10d0388
rszimmer <rdszimmer@inf.ufpel.edu.br>
2021-09-06 19:18:14 -0300

    ignore e um teste base de lista e struct

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProfilerInfo> listinha = new List<ProfilerInfo>();
+
+            listinha.Add(new ProfilerInfo(1, "um", 1));
+
+            Console.WriteLine(listinha[0].time);
+        }
+    }
+}
+
+struct ProfilerInfo {
+    public int time;
+    public string id;
+    public int inout;
+
+    public ProfilerInfo(int time, string id, int inout) {
+        this.time = time;
+        this.id = id;
+        this.inout = inout;
+
+    }
+}
